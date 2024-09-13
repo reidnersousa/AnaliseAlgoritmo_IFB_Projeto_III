7184ef83be2901bc11bc3ff183415a1dde22dbd6
reidner_sousa <79096521+reidnersousa@users.noreply.github.com>
2024-09-13 18:03:40 -0300

    Add files via upload

--- Projeto III Jacksson Yuri e Reidner/guloso_custo_beneficio copy.py
+++ Projeto III Jacksson Yuri e Reidner/guloso_custo_beneficio copy.py
@@ -1,46 +1,49 @@
+
+
 import funcoes as fc
 import os 
-import time 
-def knapsack_guloso_menor_peso(capacidade, itens):
-    # Ordena os itens pelo peso em ordem crescente
-    itens_ordenados = sorted(itens, key=lambda x: x['peso'])
+import time
 
+def knapsack_guloso_custo_beneficio(capacidade,itens):
+    
+    itens_ordenados = sorted(itens,key = lambda x : x['valor']/x['peso'],reverse = True)
     peso_total = 0
     valor_total = 0
-    mochila = []
-    vetor_solucao = [0] * len(itens)
-    for i, item in enumerate (itens_ordenados):
+    mochila =[]
+    vetor_solucao = [0] *len(itens)
+    for i,item in enumerate(itens_ordenados):
         if peso_total + item['peso'] <= capacidade:
             mochila.append(item)
-            peso_total += item['peso']
+            peso_total  += item['peso']
             valor_total += item['valor']
-            vetor_solucao[i] = 1
+
+            vetor_solucao[i]= 1
         else:
             break
-    #print("peso_total",peso_total)
     return mochila, valor_total , vetor_solucao
-
-
-
 
 
 root = r'large_scale'
 
 ### Nome do arquivo de cada instancia 
 k1,k2,k3 = fc.sepera_lotes(root)
-""" 
-print("k1",k1)
+
+print("k2",k1[2])
 nome_arquivo = k1[2]
 path_arquivo = os.path.join(root,nome_arquivo)
 itens , capacidade , saida_esperada ,y = fc.ler_instancia(path_arquivo)
-mochila , valor_total ,saida= knapsack_guloso_menor_peso(capacidade,itens)
+#print("capacidade",capacidade,"itens",itens)
+mochila , valor_total ,saida= knapsack_guloso_custo_beneficio(capacidade,itens)
+print("SS",valor_total)
 beneficio_saida_esperada = fc.calcular_beneficio(itens,saida_esperada)
 
-beneficio_saida = fc.calcular_beneficio(itens,saida)
+beneficio_saida =valor_total
 q=fc.metrica_qualidade(beneficio_saida,beneficio_saida_esperada)
 
 print("saida",beneficio_saida,"q",q)
-""" 
+
+
+
 
 def processar_arquivo_gmp(k,root):
     lista_q = []
@@ -54,33 +57,34 @@ def processar_arquivo_gmp(k,root):
 
         ## Tempo guloso menor_peso
         inicio = time.perf_counter()
-        mochila , valor_total ,saida= knapsack_guloso_menor_peso(capacidade,itens)
+        mochila , valor_total ,saida= knapsack_guloso_custo_beneficio(capacidade,itens)
         fim  = time.perf_counter()
         tempo_exc = fim - inicio
         ### Beneficio guloso menor peso
-        beneficio_gpm = valor_total
+        beneficio_cb = fc.calcular_beneficio(itens,saida)
         ### Beneficio vetor solução da última linha 
         beneficio_arquivo = fc.calcular_beneficio(itens,saida_arquivo)
 
         
-        q=fc.metrica_qualidade(beneficio_gpm,beneficio_arquivo)
+        q=fc.metrica_qualidade(beneficio_cb,beneficio_arquivo)
 
         lista_q.append(q)
         lista_y.append(y)
         lista_tempo.append(tempo_exc)
 
         
-        print("q em relação a guloso menor peso com a lista binária do arquivo:",q)
+        print("q em relação a guloso custo benficio com a lista binária do arquivo:",q)
         print("Capacidade máxima possível: ",valor_total)
         print("Tempo de execução:",tempo_exc,"segundos  Nome do arquivo:",nome_arquivo)
     
     return lista_q, lista_y, lista_tempo
 
 
-k = k3
-lista_q, lista_y, lista_tempo = processar_arquivo_gmp(k, root)
+#k = k2
+#lista_q, lista_y, lista_tempo = processar_arquivo_gmp(k, root)
 
-print("k:", k)
-print("lista_q_gmp =", lista_q)
-print("lista_y_gmp =", lista_y)
-print("lista_tempo_gmp =", lista_tempo)
+
+#print("k =", k)
+#print("lista_q_cb =", lista_q)
+#print("lista_y_cb =", lista_y)
+#print("lista_tempo_cb =", lista_tempo)
